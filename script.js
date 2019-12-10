19b925b71f6311677acb2e81953586fd23e66636
dnilsenUWT <56092346+dnilsenUWT@users.noreply.github.com>
2019-12-10 14:45:11 -0800

    Update script.js
    Layers all added

--- script.js
+++ script.js
@@ -12,28 +12,125 @@
 
 	var controlLayers = L.control.layers().addTo(map);
 
+	$.getJSON("geojson_data/fire_2010_2018.json",function(data){
+    // add GeoJSON layer to the map once the file is loaded
+    var fire2010 = L.geoJson(data, {
+			onEachFeature: function( feature, layer ){
+				layer.bindPopup( "Fire Name: " + "<strong>" + feature.properties.FIRE_NAME + "</strong><br/>" + "Year: " + feature.properties.YEAR_)
+      }
+		}).addTo(map);
+		controlLayers.addOverlay(fire2010, "2010-2018");
+	});
+
 	$.getJSON("geojson_data/fire_2000_2009.json",function(data){
     // add GeoJSON layer to the map once the file is loaded
     var fire2000 = L.geoJson(data, {
 			onEachFeature: function( feature, layer ){
-        layer.bindPopup( "<strong>" + feature.properties.FIRE_NAME + "</strong>")
+				layer.bindPopup( "Fire Name: " + "<strong>" + feature.properties.FIRE_NAME + "</strong><br/>" + "Year: " + feature.properties.YEAR_)
       }
 		}).addTo(map);
-		controlLayers.addOverlay(data, "2000-2010");
+		controlLayers.addOverlay(fire2000, "2000-2009");
 	});
 
 		$.getJSON("geojson_data/fire_1990_1999.json",function(data){
 	    // add GeoJSON layer to the map once the file is loaded
 	    var fire1990 = L.geoJson(data, {
 				onEachFeature: function( feature, layer ){
-	        layer.bindPopup( "<strong>" + feature.properties.FIRE_NAME + "</strong>")
+					layer.bindPopup( "Fire Name: " + "<strong>" + feature.properties.FIRE_NAME + "</strong><br/>" + "Year: " + feature.properties.YEAR_)
 	      }
 			}).addTo(map);
-			controlLayers.addOverlay(data, "1990-1999");
+			controlLayers.addOverlay(fire1990, "1990-1999");
 		});
 
+		$.getJSON("geojson_data/fire_1980_1989.json",function(data){
+	    // add GeoJSON layer to the map once the file is loaded
+	    var fire1980 = L.geoJson(data, {
+				onEachFeature: function( feature, layer ){
+					layer.bindPopup( "Fire Name: " + "<strong>" + feature.properties.FIRE_NAME + "</strong><br/>" + "Year: " + feature.properties.YEAR_)
+	      }
+			}).addTo(map);
+			controlLayers.addOverlay(fire1980, "1980-1989");
+		});
 
+		$.getJSON("geojson_data/fire_1970_1979.json",function(data){
+			// add GeoJSON layer to the map once the file is loaded
+			var fire1970 = L.geoJson(data, {
+				onEachFeature: function( feature, layer ){
+					layer.bindPopup( "Fire Name: " + "<strong>" + feature.properties.FIRE_NAME + "</strong><br/>" + "Year: " + feature.properties.YEAR_)
+				}
+			}).addTo(map);
+			controlLayers.addOverlay(fire1970, "1970-1979");
+		});
 
+		$.getJSON("geojson_data/fire_1960_1969.json",function(data){
+			// add GeoJSON layer to the map once the file is loaded
+			var fire1960 = L.geoJson(data, {
+				onEachFeature: function( feature, layer ){
+					layer.bindPopup( "Fire Name: " + "<strong>" + feature.properties.FIRE_NAME + "</strong><br/>" + "Year: " + feature.properties.YEAR_)
+				}
+			}).addTo(map);
+			controlLayers.addOverlay(fire1960, "1960-1969");
+		});
+
+		$.getJSON("geojson_data/fire_1950_1959.json",function(data){
+			// add GeoJSON layer to the map once the file is loaded
+			var fire1950 = L.geoJson(data, {
+				onEachFeature: function( feature, layer ){
+					layer.bindPopup( "Fire Name: " + "<strong>" + feature.properties.FIRE_NAME + "</strong><br/>" + "Year: " + feature.properties.YEAR_)
+				}
+			}).addTo(map);
+			controlLayers.addOverlay(fire1950, "1950-1959");
+		});
+
+		$.getJSON("geojson_data/fire_1940_1949.json",function(data){
+			// add GeoJSON layer to the map once the file is loaded
+			var fire1940 = L.geoJson(data, {
+				onEachFeature: function( feature, layer ){
+					layer.bindPopup( "Fire Name: " + "<strong>" + feature.properties.FIRE_NAME + "</strong><br/>" + "Year: " + feature.properties.YEAR_)
+				}
+			}).addTo(map);
+			controlLayers.addOverlay(fire1940, "1940-1949");
+		});
+
+		$.getJSON("geojson_data/fire_1930_1939.json",function(data){
+			// add GeoJSON layer to the map once the file is loaded
+			var fire1930 = L.geoJson(data, {
+				onEachFeature: function( feature, layer ){
+					layer.bindPopup( "Fire Name: " + "<strong>" + feature.properties.FIRE_NAME + "</strong><br/>" + "Year: " + feature.properties.YEAR_)
+				}
+			}).addTo(map);
+			controlLayers.addOverlay(fire1930, "1930-1939");
+		});
+
+		$.getJSON("geojson_data/fire_1920_1929.json",function(data){
+			// add GeoJSON layer to the map once the file is loaded
+			var fire1920 = L.geoJson(data, {
+				onEachFeature: function( feature, layer ){
+					layer.bindPopup( "Fire Name: " + "<strong>" + feature.properties.FIRE_NAME + "</strong><br/>" + "Year: " + feature.properties.YEAR_)
+				}
+			}).addTo(map);
+			controlLayers.addOverlay(fire1920, "1920-1929");
+		});
+
+		$.getJSON("geojson_data/fire_1910_1919.json",function(data){
+			// add GeoJSON layer to the map once the file is loaded
+			var fire1910 = L.geoJson(data, {
+				onEachFeature: function( feature, layer ){
+					layer.bindPopup( "Fire Name: " + "<strong>" + feature.properties.FIRE_NAME + "</strong><br/>" + "Year: " + feature.properties.YEAR_)
+				}
+			}).addTo(map);
+			controlLayers.addOverlay(fire1910, "1910-1919");
+		});
+
+		$.getJSON("geojson_data/fire_1900_1909.json",function(data){
+			// add GeoJSON layer to the map once the file is loaded
+			var fire1900 = L.geoJson(data, {
+				onEachFeature: function( feature, layer ){
+					layer.bindPopup( "Fire Name: " + "<strong>" + feature.properties.FIRE_NAME + "</strong><br/>" + "Year: " + feature.properties.YEAR_)
+				}
+			}).addTo(map);
+			controlLayers.addOverlay(fire1900, "1900-1909");
+		});
 	// var overlays = {
 	// 	"2000-2010": fire2000,
 	// 	"1990-1999": fire1999,
